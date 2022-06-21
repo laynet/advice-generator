e72a958279f68e59fc630f9094ccac6bdff3badb
Layne <layneingramtaylor@gmail.com>
2022-06-21 10:17:37 -0500

    initial commit: app complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import mobileDivider from './assets/pattern-divider-mobile.svg';
+import desktopDivider from './assets/pattern-divider-desktop.svg';
+import diceBtn from './assets/icon-dice.svg';
+import './App.scss';
 
+const url = 'https://api.adviceslip.com/advice';
 function App() {
+  const [id, setID] = useState('');
+  const [quote, setQuote] = useState('');
+
+  const fetchData = () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setID(data.slip.id);
+        setQuote(data.slip.advice);
+      });
+    // console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='card'>
+        <h4 className='eyebrow'>Advice #{id}</h4>
+        <h1 className='quote'>"{quote}"</h1>
+        <img className='mobile-divider' src={mobileDivider} alt='' />
+        <img className='desktop-divider' src={desktopDivider} alt='' />
+        <button className='btn' onClick={fetchData}>
+          <img src={diceBtn} alt='' />
+        </button>
+      </div>
+    </>
   );
 }
 
